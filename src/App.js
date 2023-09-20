6aeacbbac3c8eeebf1b6e990655f75639096844a
Ritesh <codeshorts007@gmail.com>
2023-09-20 17:21:23 +0530

    addFile- formating setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Resume from "./pages/Resume";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Project />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Resume.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Resume() {
+  return <div></div>;
+}
+
+export default Resume;
