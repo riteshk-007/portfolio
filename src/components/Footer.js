e91c12b24ffd4dffd2820d3d88276a873b3ec4b0
Ritesh <codeshorts007@gmail.com>
2023-09-21 14:31:02 +0530

    style- design footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { BsGithub } from "react-icons/bs";
+import { RiTwitterXFill } from "react-icons/ri";
+import { FaLinkedinIn } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-evenly bg-[#0F0416] text-white flex-wrap px-3 py-4">
+      <p className="text-sm text-center my-2">
+        Designed and Developed by Soumyajit Behera
+      </p>
+      <p className="text-sm font-semibold text-center my-2">
+        Copyright © 2023 Ritesh.dev
+      </p>
+      <span className="flex items-center justify-center gap-7 my-2">
+        <a
+          href="https://github.com/riteshk-007"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white"
+        >
+          <BsGithub />
+        </a>
+        <a
+          href="https://twitter.com/Riteshk_007"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white"
+        >
+          <RiTwitterXFill />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ritesh-kumar-8064b626b"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white"
+        >
+          <FaLinkedinIn />
+        </a>
+        <a
+          href="https://www.instagram.com/riteshk_007/?igshid=MzNlNGNkZWQ4Mg%3D%3D"
+          target="_blank"
+          rel="noreferrer"
+          className="text-white"
+        >
+          <AiFillInstagram />
+        </a>
+      </span>
+    </div>
+  );
 }
 
 export default Footer;
