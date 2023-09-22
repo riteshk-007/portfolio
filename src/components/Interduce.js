271467e4bac2f66d196415d18e5166b2da672b8f
Ritesh <codeshorts007@gmail.com>
2023-09-22 16:51:18 +0530

    style- interduce home page section design

---
+++ src/components/Interduce.js
@@ -0,0 +1,61 @@
+import React from "react";
+import Tilt from "react-parallax-tilt";
+
+function Interduce() {
+  return (
+    <div className="flex flex-wrap items-center justify-center flex-col lg:flex-row relative overflow-hidden">
+      <img
+        src="./star.jpg"
+        alt=""
+        className="h-full w-full object-cover opacity-10 absolute bottom-0"
+      />
+      <div className="w-full lg:w-1/2 h-1/2 lg:h-full flex items-center justify-center flex-col my-10 lg:gap-10">
+        <h1 className="flex text-white text-2xl lg:text-4xl gap-2 my-10">
+          LET ME <p className="text-[#ad50eb]">INTRODUCE</p> MYSELF
+        </h1>
+        <div className="flex items-center m-auto justify-center w-11/12 lg:w-3/4 lg:mr-10">
+          <span className="flex flex-col gap-3 text-white items-start  text-base justify-center font-semibold lg:text-lg">
+            <p>
+              I fell in love with coding and I have at least learnt something, I
+              think… 🤷‍♂️
+            </p>
+
+            <p>
+              I am fluent in classics like{" "}
+              <span className="text-[#ad50eb]">React JS.</span>
+            </p>
+
+            <p>
+              My field of Interest's are building new{" "}
+              <span className="text-[#ad50eb]">Website</span> And{" "}
+              <span className="text-[#ad50eb]">Web App</span> .
+            </p>
+
+            <p>
+              Whenever possible, I also apply my passion for developing products
+              with{" "}
+              <span className="text-[#ad50eb]">Modern Javascript Library</span>{" "}
+              and <span className="text-[#ad50eb]">Frameworks </span>
+              like
+              <span className="text-[#ad50eb]"> React.js.</span>
+            </p>
+            <p>
+              And I will soon be able to create a{" "}
+              <span className="text-[#ad50eb]">website </span> and{" "}
+              <span className="text-[#ad50eb]"> web app</span> with
+              <span className="text-[#ad50eb]"> Nextjs</span> and{" "}
+              <span className="text-[#ad50eb]">Typescript</span>.
+            </p>
+          </span>
+        </div>
+      </div>
+      <div className="w-full lg:w-1/2 h-full flex items-center justify-center p-10">
+        <Tilt>
+          <img src="man.png" alt="" className="object-cover" />
+        </Tilt>
+      </div>
+    </div>
+  );
+}
+
+export default Interduce;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero";
+import Interduce from "../components/Interduce";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Interduce />
     </div>
   );
 }
