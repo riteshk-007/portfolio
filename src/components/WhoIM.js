a300e083a4621266f0778a2b94eb45b0a1068504
Ritesh <codeshorts007@gmail.com>
2023-09-28 17:10:32 +0530

    style-  Update WhoIM component layout

--- src/components/WhoIM.js
+++ src/components/WhoIM.js
@@ -12,33 +12,25 @@ function WhoIM() {
         <h1 className="flex text-white text-2xl lg:text-4xl gap-2 font-bold">
           Know Who <p className="text-[#ad50eb]">I'M</p>
         </h1>
-        <div className="flex items-center m-auto justify-center w-11/12 lg:w-3/4">
-          <span className="flex flex-col gap-3 text-white items-start  text-base justify-center font-semibold lg:text-lg lg:mr-20">
-            <p className="text-center mt-3">
+        <div className="flex items-center m-auto justify-center w-11/12 lg:w-4/5">
+          <span className="flex flex-col gap-3 text-white items-start  text-base justify-center font-semibold lg:text-lg">
+            <p className="text-center mt-3 mx-auto md:mx-0">
               Hi Everyone, I am <span className="text-[#ad50eb]"> Ritesh</span>{" "}
-              from <span className="text-[#ad50eb]">Delhi, India.</span>
+              from <span className="text-[#ad50eb]">Delhi, India</span>
             </p>
-            <p className="lg:text-xl font-thin">
-              I am a 3<sup>rd</sup> year bcom student at Delhi University, and I
-              am making a career in{" "}
+            <p className="lg:text-xl font-thin ">
+              Greetings everyone! I'm Ritesh, a third-year BCom student at Delhi
+              University. My passion lies in{" "}
               <span className="text-[#ad50eb] font-semibold">
                 web development
-              </span>{" "}
-              and I am always looking for new challenges. I believe that I can
-              use my <span className="text-[#ad50eb] font-light">skills </span>
-              and <span className="text-[#ad50eb] font-light">
-                experience{" "}
-              </span>{" "}
-              to contribute to any team and help you
-              <span className="text-[#ad50eb] font-light"> achieve </span>
-              your<span className="text-[#ad50eb] font-light"> goals.</span> I
-              am looking for an opportunity where I can use my skills in
-              contributing to the{" "}
-              <span className="text-[#ad50eb] font-light"> success </span> of
-              the company and also to improve my{" "}
-              <span className="text-[#ad50eb] font-light">personal skills</span>
+              </span>
+              , and I'm always up for a new challenge. With my skills and
+              experience, I believe that I can make a valuable contribution to
+              any team and help achieve our shared goals. I'm seeking an
+              opportunity where I can apply my abilities to drive the company's
+              success while also enhancing my personal growth.
             </p>
-            <p className="flex text-gray-200 text-base lg:text-lg gap-2 font-semibold">
+            <p className="flex text-gray-200 text-base lg:text-lg gap-2 font-semibold ">
               Apart from coding, some other activities that I love to do!
             </p>
             <span className="flex items-center justify-center gap-3 font-sans text-gray-200">
