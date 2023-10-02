84c11279246535a19b4838b1c1655504aec94c6b
Ritesh <codeshorts007@gmail.com>
2023-10-02 13:35:13 +0530

    update- resume and skill

--- src/components/Techstack.js
+++ src/components/Techstack.js
@@ -14,6 +14,7 @@ import {
   SiTailwindcss,
   SiGreensock,
   SiJquery,
+  SiPwa,
 } from "react-icons/si";
 import { BsGit } from "react-icons/bs";
 
@@ -59,6 +60,9 @@ const data = [
   },
   {
     icon: <FaWordpress fontSize={70} color="#fff" />,
+  },
+  {
+    icon: <SiPwa fontSize={70} color="#fff" />,
   },
 ];
 function Techstack() {
