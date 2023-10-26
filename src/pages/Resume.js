12d67cba25d1e0eee66fe416a24f1f0ce703bebe
Ritesh <codeshorts007@gmail.com>
2023-10-26 16:54:46 +0530

    resume change

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -18,7 +18,7 @@ function Resume() {
         className="h-full w-full object-cover opacity-10 absolute bottom-0"
       />
       <a
-        href={"../../rk-PDF.pdf"}
+        href={"../../Ritesh.pdf"}
         target="_blank"
         rel="noreferrer"
         className="flex items-center z-20 justify-center gap-3 px-6 py-2 rounded  shadow-md text-white bg-purple-600 hover:bg-purple-700 transition duration-300 ease-in-out my-3"
@@ -27,14 +27,14 @@ function Resume() {
       </a>
       <div className="py-[50px] justify-center flex items-center overflow-hidden z-20">
         <Document
-          file="../../rk-PDF.pdf"
+          file="../../Ritesh.pdf"
           className="flex justify-center shadow-xl"
         >
           <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
         </Document>
       </div>
       <a
-        href={"../../rk-PDF.pdf"}
+        href={"../../Ritesh.pdf"}
         target="_blank"
         rel="noreferrer"
         className="flex items-center z-20 justify-center gap-3 px-6 py-2 rounded  shadow-md text-white bg-purple-600 hover:bg-purple-700 transition duration-300 ease-in-out my-3"
